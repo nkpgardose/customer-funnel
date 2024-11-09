c96b8cc301f20365f17dc0e3a1ba923a52941ca1
Neil <nkpgardose@gmail.com>
2024-11-10 03:59:40 +1100

    Add zod resolver for react hooks; setup error simple behaviour

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 import styles from './App.module.css'
-import { useForm } from "react-hook-form"
 
-interface IPersonalDetailsInput {
-  firstName: string,
-  lastName: string,
-  email: string,
-  employmentStatus: string,
+enum EmploymentStatuses {
+  Employed = "Employed",
+  Unemployed = "Unemployed",
+  SelfEmployed = 'SelfEmployed',
 }
+
+const EmploymentOptions: Record<EmploymentStatuses, string> = {
+  [EmploymentStatuses.Employed]: "Employed",
+  [EmploymentStatuses.Unemployed]: "Unemployed",
+  [EmploymentStatuses.SelfEmployed]: "Self-Employed",
+}
+
+const personalFormSchema = z.object({
+  firstName: z
+    .string()
+    .min(1, { message: 'Please provide your first name.' }),
+  lastName: z
+    .string()
+    .min(1, { message: 'Please provide your last name.' }),
+  email: z
+    .string()
+    .min(1, { message: 'Please provide your email address.' })
+    .email({ message: 'Please enter a valid email address.' }),
+  employmentStatus: z.nativeEnum(EmploymentStatuses),
+});
 
 function App() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<IPersonalDetailsInput>()
+  } = useForm<z.infer<typeof personalFormSchema>>({
+    resolver: zodResolver(personalFormSchema)
+  })
 
-  function onSubmit(data: IPersonalDetailsInput) {
+  function onSubmit(data: z.infer<typeof personalFormSchema>) {
     console.log(data);
   }
 
@@ -26,36 +49,32 @@ function App() {
         <form onSubmit={handleSubmit(onSubmit)}>
           <div>
             <label htmlFor="first-name-field">First Name</label>
-            <input type="text" id="first-name-field" {...register('firstName', { required: 'Please provide your first name.' })} />
-            {errors.firstName && <span>{errors.firstName.message}</span>}
+            <input type="text" id="first-name-field" {...register('firstName')} />
+            {errors.firstName?.message && <p>{errors.firstName.message}</p>}
           </div>
           <div>
             <label htmlFor="last-name-field">Last Name</label>
             <input
               type="text"
               id="last-name-field"
-              {...register('lastName', {
-                required: "Please provide your last name",
-                maxLength: {
-                  value: 2,
-                  message: "Min 2"
-                }
-              })}
+              {...register('lastName')}
             />
-            {errors.lastName && <span>{errors.lastName.message}</span>}
+            {errors.lastName?.message && <p>{errors.lastName.message}</p>}
           </div>
           <div>
             <label htmlFor="email-field">Email</label>
-            <input type="email" id="email-field" {...register('email', { required: true  })} />
-            {errors.email && <span>{errors.email.message}</span>}
+            <input type="email" id="email-field" {...register('email')} />
+            {errors.email?.message && <p>{errors.email.message}</p>}
           </div>
           <div>
             <label htmlFor="employment-status-select">Employment Status</label>
-            <select id="employment-status-select" {...register('employmentStatus', { required: true })}>
+            <select id="employment-status-select" {...register('employmentStatus')}>
               <option value="">Open to see selections</option>
-              <option value="employed">Employed</option>
-              <option value="selfEmployed">Self-Employed</option>
-              <option value="unemployed">Unemployed</option>
+              {Object.entries(EmploymentOptions).map(item => {
+                return (
+                  <option value={item[0]}>{item[1]}</option>
+                );
+              })}
             </select>
             {errors.employmentStatus && <span>Please select your employment status.</span>}
           </div>
